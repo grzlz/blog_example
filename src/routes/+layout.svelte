b9e9d77d2315ca2064f5e03dac64ae55fbe994d7
grzlz <guillermo.rdzlpz@gmail.com>
2025-05-22 00:18:40 -0600

    Feat: animate dropdown of hamburger menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
+	import { slide } from 'svelte/transition';
 	import '../app.css';
 	let { children } = $props();
 	let mobileMenuOpen = $state(false);
 </script>
 
 <div class="flex flex-col min-h-screen">
-	<nav class="flex justify-between items-center px-6 py-4 bg-secondary-50 text-secondary-600 sticky top-0 z-50">
+	<nav class="sticky flex justify-between items-center px-6 py-4 bg-secondary-50 text-secondary-600  top-0 z-10 border-b border-primary-800">
 						<div class="h-14 w-14 mr-3">
 					<img
 						src="/logo.png"
@@ -21,19 +22,21 @@
 		</ul>
 
 		<!-- Mobile menu button -->
-		<button class="md:hidden" onclick={() => mobileMenuOpen = !mobileMenuOpen}>
+		<button class="md:hidden text-3xl" onclick={() => mobileMenuOpen = !mobileMenuOpen}>
 			☰
 		</button>
+		{#if mobileMenuOpen}
+			<ul class="absolute top-full left-0 w-full z-20 bg-secondary-100 text-secondary-700 md:hidden shadow-lg rounded-b-lg p-4"
+				in:slide={{ duration: 250 }}
+				out:slide={{ duration: 200 }}>
+				<li class="hover:text-secondary-900">Home</li>
+				<li class="hover:text-secondary-900">About</li>
+				<li class="hover:text-secondary-900">Contact</li>
+			</ul>
+		{/if}
 	</nav>
 
 	<!-- Mobile dropdown menu -->
-	{#if mobileMenuOpen}
-		<ul class="flex flex-col gap-2 px-6 py-4 bg-secondary-100 text-secondary-600 md:hidden">
-			<li class="hover:text-secondary-800">Home</li>
-			<li class="hover:text-secondary-800">About</li>
-			<li class="hover:text-secondary-800">Contact</li>
-		</ul>
-	{/if}
 
 	{@render children()}
 
